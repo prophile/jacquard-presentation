67993455263792a9b5e83325445c6ea333fe9fd0
Alistair Lynn <arplynn@gmail.com>
2018-04-28 13:42:13 +0100

    Flush on reset

--- demo/experiments/defaults.sh
+++ demo/experiments/defaults.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
+jacquard storage-flush --force
 jacquard set-default lp_header Pricing
 jacquard set-default lp_free_cta "Sign up for free"
 jacquard set-default lp_pro_cta "Get started"

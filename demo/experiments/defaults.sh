8cdf330b66e9b5a4324335a1a9cd1a80b57a854b
Alistair Lynn <arplynn@gmail.com>
2018-04-28 11:12:38 +0100

    Add experiment settings for the demo

---
+++ demo/experiments/defaults.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+jacquard set-default lp_header Pricing
+jacquard set-default lp_free_cta "Sign up for free"
+jacquard set-default lp_pro_cta "Get started"
+jacquard set-default lp_enterprise_cta "Contact us"
+
